43795cb79e2f89a0fdad8d20f607252a26c82684
Cristina <cristina.corralm@gmail.com>
2017-11-30 10:46:46 +0100

    Update onebot.py
    last commit had lots of errors

--- onebot.py
+++ onebot.py
@@ -1,9 +1,10 @@
+# -*- encoding: utf 8 -*-
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import ChatMember
 import logging
 from random import shuffle
-import Player
-import Game
+from Player import Player
+from Game import Game
 
 
 # Enable logging
@@ -14,10 +15,11 @@ logger = logging.getLogger(__name__)
 TOKEN = "___SUPERSECRETTOKEN__" #write here your bot's token
 GREETINGS = 'Welcome to One Poker. If you wish to participate, enter the command /participate'
 GREETINGS2 = 'Type /rules for an explanation of the game mechanics and /help to know the commands of this bot. Type /disclaimer to know further.'
-RULES = 'One Poker is a game in which two people play following these rules:\n1. Both of you will receive 10 lifes, you can bet a minimum of 1 life in each round and a maximum of the lifes you have at that moment.\n2.The game uses three decks of poker without the joker card, when . \n Each player will receive 2 cards and will have in his hand always 2 cards. \n The value of the cards is determined by its number, being DOWN cards: 2, 3, 4, 5, 6, 7 and UP cards: 8, 9, 10, J, Q, K, A. \n The winning card is always the highest with the exception of the 2 that wins the Ace. \n Once players receive their cards, both of them will be informed if they have UP or DOWN cards in their hand. \n The game ends when one of the players loses all his lifes.\n '
+RULES = 'One Poker is a game in which two people play following these rules:\n1. Both of you will receive 10 lifes, you can bet a minimum of 1 life in each round and a maximum of the lifes you have at that moment.\n2.The game uses three decks of poker without the joker card, when . \n 3. Each player will receive 2 cards and will have in his hand always 2 cards. \n 4. The value of the cards is determined by its number, being DOWN cards: 2, 3, 4, 5, 6, 7 and UP cards: 8, 9, 10, J, Q, K, A. \n 5. The winning card is always the highest with the exception of the 2 that wins the Ace. \n 6. Once players receive their cards, both of them will be informed if they have UP or DOWN cards in their hand. \n 7. The game ends when one of the players loses all his lifes.\n '
 HELP = 'Here is a list of all the commands Mother Sophie has:\n /disclaimer\n /endgame\n /freeasinfreedom - Link to the code in github.\n /fold\n /help - This command.\n /participate - Starts a new game.\n /quit - Players cannot quit One Poker irl but here you can.\n /raise\n /rules\n /scores - Display how the game goes so far, including cards, victories\n /start\n /state\n /zawa\n'
 DISCLAIMER = 'I don not own any of names or refences made of the manga neither the original gameplay idea of One Poker.'
 FREEDOM = 'If you want a copy of the code, you can have it in my github: https://github.com/cryogenic-dreams/one_poker_bot/'
+chats={} #this shit right here is to have a control over games and chats
 
 
 def start(bot, update):
@@ -39,35 +41,52 @@ def disclaimer(bot, update):
     
 def freeasinfreedom(bot, update):
     """Send a message when the command /rules is issued."""
-    update.message.reply_text(DISCLAIMER)
+    bot.send_message(update.message.chat_id, FREEDOM)
 
 def status(bot, update, hand1, hand2):
     """Send the UPs and DOWNs of the players when the command /status is issued."""
-    update.message.reply_text()
-    update.message.reply_text()
+    chat_id = update.message.chat_id
+    update.message.reply_text(chats[chat_id].player1.displayStatus())
+    update.message.reply_text(chats[chat_id].player2.displayStatus())
 
 def participate(bot, update):
     """take id and name of the user"""
     """after second input, start the actual game"""
-    update.message.reply_text('Hello player1')
+    chat_id = update.message.chat_id
+    name = update.message.from_user.first_name
+    if (not(chats.has_key(chat_id))):
+        update.message.reply_text('Hello ' + name + ', you are player 1')
+	p1 = Player(name)
+	game = Game(chat_id, p1, None, [], [])
+    	chats[chat_id] = game
+    else:
+	if(chats[chat_id].player2 is None):
+	    p2 = Player(name)
+	    chats[chat_id].player2 = p2
+    	    update.message.reply_text('Hello ' + name + ' you are player 2')
+	    #once the players are set, lets give em cards
+	    chats[chat_id].giveCards(1)
+	else:
+	    update.message.reply_text('There is a maximum of 2 players in this game.')
+    print chats
 	
 def raiseBet(bot, update):
-	"""Raise a bet"""
+    """Raise a bet"""
 	
 def fold(bot, update):
-	"""Fold"""
+    """Fold"""
 
 def quit(bot, update):
-	"""Player that sends the message quits the game"""
+    """Player that sends the message quits the game"""
 	
 def scores(bot, update):
-	"""Prints all the matches in a game"""
+    """Prints all the matches in a game"""
 
 def endgame(bot, update):
-	"""End the game, like quit, but there are not winners nor losers"""
+    """End the game, like quit, but there are not winners nor losers"""
 	
-def endgame(bot, update):
-	"""Every 1 to 10 minutes (randomly) bot posts a zawa (ざわ) message. It's a switcher, typing it for the 1st enables and the 2nd time disables it"""
+def zawa(bot, update):
+    """Every 1 to 10 minutes (randomly) bot posts a zawa (ざわ) message. It's a switcher, typing it for the 1st enables and the 2nd time disables it"""
 	
 def echo(bot, update):
     """Lets keep the tutorial's examples from now"""
@@ -80,7 +99,7 @@ def error(bot, update, error):
 
 def main():
 
-   updater = Updater(TOKEN)
+    updater = Updater(TOKEN)
     dp = updater.dispatcher
 
     # on different commands - answer in Telegram
@@ -105,3 +124,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
