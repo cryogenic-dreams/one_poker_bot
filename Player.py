0d1dc26b9dab031b1961887c7c6872ff04878a1e
Cristina <cristina.corralm@gmail.com>
2017-11-30 12:18:39 +0100

    Update Player.py
    plural of life is lives

--- Player.py
+++ Player.py
@@ -1,16 +1,16 @@
 class Player:
-	LIFES = 10
+	LIVES = 10
 	BET = 0
 
 	def __init__ (self, name):
 		self.hand = []
 		#self.id = id
 		self.name = name
-		self.lifes = Player.LIFES
+		self.lives = Player.LIVES
 		self.bet = Player.BET
 	
 	def displayStatus(self):
-		return 'Player ' + self.name + ': ' + self.isUPorDOWN(self.hand[0]) + ' | ' + self.isUPorDOWN(self.hand[1])
+		return self.name + ': ' + self.isUPorDOWN(self.hand[0]) + ' | ' + self.isUPorDOWN(self.hand[1])
 	
 	def isUPorDOWN(card): #ideally, this will be in a different class named card, but... 2lazy
 		if ('2' in card or '3' in card or '4' in card or '5' in card or '6' in card or '7' in card):
@@ -19,8 +19,8 @@ class Player:
 			return 'UP'
 
 	def setBet(self, quantity):
-		if (quantity <= self.lifes):
-			self.lifes = self.lifes - quantity
+		if (quantity <= self.lives):
+			self.lives = self.lives - quantity
 			self.bet = self.bet + quantity
 		else:
 			return false
