5155a245ac1b49efed3cc454e31298ac1f1de531
Cristina <cristina.corralm@gmail.com>
2017-12-13 00:30:58 +0100

    Update Game.py

--- Game.py
+++ Game.py
@@ -4,7 +4,8 @@ from Player import Player
 
 
 class Game:
-    # I heard you like emojis, so I put emojis in the code so you can enjoy them while playing. ur welcome
+    # I heard you like emojis, so I put unicode in the code so you can enjoy them while playing. ur welcome
+
     clubs = ['2 of ♣️', '3 of ♣️', '4 of ♣️', '5 of ♣️', '6 of ♣️', '7 of ♣️', '8 of ♣️', '9 of ♣️', '10 of ♣️',
              'Jack of ♣️', 'Queen of ♣️', 'King of ♣️', 'Ace of ♣️']
     spades = ['2 of ♠️', '3 of ♠️', '4 of ♠️', '5 of ♠️', '6 of ♠️', '7 of ♠️', '8 of ♠️', '9 of ♠️', '10 of ♠️',
@@ -16,17 +17,25 @@ class Game:
     DECK = clubs + spades + hearts + diamonds
     GAME_DECK = DECK * 3
 
-    def __init__(self, chat_id, player1, player2, matches, victories):
+    def __init__(self,
+                 chat_id,
+                 player1,
+                 player2=None,
+                 matches=[],
+                 victories=[],
+                 bets=[]):
+
         self.chat_id = chat_id
         self.player1 = player1
         self.player2 = player2
         self.deck = Game.GAME_DECK
         shuffle(self.deck)
         self.matches = matches
+        self.bets = bets
         self.victories = victories
 
-    def giveCards(self, firstP):
-        if firstP == 1:
+    def giveCards(self, first_player):
+        if first_player == 1:
             self.player1.hand.append(self.giveCard())
             self.player2.hand.append(self.giveCard())
         else:
@@ -37,14 +46,17 @@ class Game:
         if len(self.deck) != 0:
             return self.deck.pop()
         else:
-            # end game here
-            return false
+            # never came to this end, so reshuffle and chill
+            self.deck = Game.GAME_DECK
+            shuffle(self.deck)
+            return self.deck.pop()
 
     def winningCard(self, card1, card2):
         """
-		Returns 1 if first card wins or 2 if second card wins
-		Returns 0 if there's a tie
-		"""
+        Returns 1 if first card wins or 2 if second card wins
+        Returns 0 if there's a tie
+        """
+
         self.matches.append([card1, card2])
 
         if '♣️' in card1:
@@ -78,9 +90,57 @@ class Game:
             else:
                 return 1
 
-    def manageResult(self, card1, card2):  # save results and redistribute lives
-        result = self.winningCard(card1, card2)
+    def manageResult(self, card1, card2, fold=None):  # save results and redistribute lives
+        if fold is None:
+            result = self.winningCard(card1, card2)
+        else:
+            result = fold  # technically this should be called winner, not fold
         self.victories.append(result)
+        self.manageLives(result)
+        self.initPlayers()
+        self.manageCards(result)
+
+        return self.displayScore(len(self.victories)-1)
+
+    def initPlayers(self):
+        self.player1.check = False
+        self.player2.check = False
+        self.player1.bet = 0
+        self.player1.setBet(1)
+        self.player2.bet = 0
+        self.player2.setBet(1)
+        self.player1.card_played = []
+        self.player2.card_played = []
+
+    def displayScores(self):
+        i = len(self.victories) - 1
+        string = ""
+        while i > -1:
+            string = self.displayScore(i) + string
+            i -= 1
+        return string
+
+    def displayScore(self, number):
+        return "`Round %i: [%s(bet: %i) - %s | %s(bet:%i) - %s] RESULT: %s`" % (
+            number + 1, self.player1.name, self.bets[number][0],
+            self.matches[number][0].decode('utf-8'),
+            self.player2.name, self.bets[number][1],
+            self.matches[number][1].decode('utf-8'),
+            self.whoWon(self.victories[number]))
+
+    def whoWon(self, number):
+        """
+        Players are just numbers, after all...
+        """
+        if number == 1:
+            return self.player1.name + " won.\n"
+        elif number == 2:
+            return self.player2.name + " won.\n"
+        else:
+            return "Players tied.\n"
+
+    def manageLives(self, result):
+        self.bets.append([self.player1.bet, self.player2.bet])
         if result == 1:
             # player 1 wins
             self.player1.lives = self.player1.lives + self.player1.bet + self.player2.bet
@@ -92,41 +152,10 @@ class Game:
             self.player1.lives = self.player1.lives + self.player1.bet
             self.player2.lives = self.player2.lives + self.player2.bet
 
-        self.initPlayers()
+    def manageCards(self, result):
         if result == 1:
             self.giveCards(1)
         elif result == 2:
             self.giveCards(2)
         else:
             self.giveCards(1)
-
-        return self.whoWon(result)
-
-    def initPlayers(self):
-        self.player1.check = False
-        self.player2.check = False
-        self.player1.bet = 0
-        self.player2.bet = 0
-        self.player1.card_played = []
-        self.player2.card_played = []
-
-    def displayScores(self):
-        i = len(self.victories)-1
-        string = ''
-        while i > -1:
-            string = '``` Round %i: [%s - %s | %s - %s] RESULT: %s ```' % (
-                i + 1, self.player1.name, self.matches[i][0].decode('utf-8'), self.player2.name, self.matches[i][1].decode('utf-8'),
-                self.whoWon(self.victories[i])) + string
-            i -= 1
-        return string
-
-    def whoWon(self, number):
-        """
-		Players are just numbers, after all...
-		"""
-        if number == 1:
-            return self.player1.name + ' won.\n'
-        elif number == 2:
-            return self.player2.name + ' won.\n'
-        else:
-            return 'Players tied.\n'
