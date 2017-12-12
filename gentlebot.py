d225711f7b359bb35e0941177af54f857bb466a0
Cristina <cristina.corralm@gmail.com>
2017-12-13 00:34:17 +0100

    added more waiting time

--- gentlebot.py
+++ gentlebot.py
@@ -2,14 +2,17 @@ import telegram.bot
 from telegram.ext import messagequeue as mq
 from strings import Strings
 from telegram.utils.request import Request
+import time
+
 
 class MQBot(telegram.bot.Bot):
     """
     A subclass of Bot which delegates send method handling to MQ
     GentleBot is gentle with messages so telegram doesn't ban it
     """
+
     def __init__(self, is_queued_def=True, mqueue=None, *args, **kwargs):
-        super(MQBot, self).__init__(Strings.TOKEN, request=Request(con_pool_size=8), **kwargs)
+        super(MQBot, self).__init__(Strings.TOKEN, request=Request(con_pool_size=8), *args, **kwargs)
         # below 2 attributes should be provided for decorator usage
         self._is_messages_queued_default = is_queued_def
         self._msg_queue = mqueue or mq.MessageQueue()
@@ -24,7 +27,11 @@ class MQBot(telegram.bot.Bot):
     @mq.queuedmessage
     def send_message(self, *args, **kwargs):
         """
+        :param args:
+        :param kwargs:
+        :return:
         Wrapped method would accept new `queued` and `isgroup`
         OPTIONAL arguments
         """
+        time.sleep(.5)
         super(MQBot, self).send_message(*args, **kwargs)
