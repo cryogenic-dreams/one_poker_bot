a249340fca4b46ba14c994e8bbd2d297602553c2
Cristina <cristina.corralm@gmail.com>
2017-12-13 00:28:23 +0100

    Update Player.py
    Now it's more beautiful

--- Player.py
+++ Player.py
@@ -1,25 +1,31 @@
 class Player:
-    LIVES = 10
-    BET = 0
 
-    def __init__(self, name, user, card, check, reply_id):
-        self.hand = []
+    def __init__(self,
+                 name,
+                 user,
+                 reply_id,
+                 lives=9,
+                 bet=1,
+                 card=[],
+                 check=False,
+                 red_lives=1,
+                 hand=[]):
+        
         self.user = user
-        # self.id = id
         self.name = name
-        self.lives = Player.LIVES
-        self.bet = Player.BET
+        self.lives = lives
+        self.bet = bet
         self.card_played = card
         self.check = check
         self.reply_id = reply_id
-        self.red_lives = 1
+        self.red_lives = red_lives
+        self.hand = hand
 
     def displayStatus(self):
         if len(self.hand) > 1:
-            return '``` ' + self.name + ': ' + self.isUPorDOWN(self.hand[0]) + ' | ' + self.isUPorDOWN(
-                self.hand[1]) + '```'
+            return "`%s: %s | %s`" % (self.name, self.isUPorDOWN(self.hand[0]), self.isUPorDOWN(self.hand[1]))
         else:
-            return ' '
+            return " "
 
     def isUPorDOWN(self, card):  # ideally, this will be in a different class named card, but... 2lazy
         if '2' in card or '3' in card or '4' in card or '5' in card or '6' in card or '7' in card:
