0d1c6eac99a3182ff0417f73648206f30f1869a5
Cristina <cristina.corralm@gmail.com>
2017-12-20 06:22:57 +0100

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 class Strings:
 
-  TOKEN = "___YOURBOTSTOKEN___" #write here your bot's token
+  TOKEN = "500680213:AAH6o1YqqkFHg8MyBRRKZpfQGsu_e0OZ9Jw" #write here your bot's token
   GREETINGS = "`Welcome to One Poker. If you wish to participate, enter the command` /participate"
   GREETINGS2 = "`Type` /rules `for an explanation of the game mechanics and` /help `to know the commands of this bot. Type` /disclaimer `to know further.`"
   RULES = "One Poker is a game in which two people play following these rules:\n1. Both of you will receive 10 lives, you can bet a minimum of 1 life in each round and a maximum of the lives you have at that moment.\n2.The game uses three decks of poker without the joker card, when . \n 3. Each player will receive 2 cards and will have in his hand always 2 cards. \n 4. The value of the cards is determined by its number, being DOWN cards: 2, 3, 4, 5, 6, 7 and UP cards: 8, 9, 10, J, Q, K, A. \n 5. The winning card is always the highest with the exception of the 2 that wins the Ace. \n 6. Once players receive their cards, both of them will be informed if they have UP or DOWN cards in their hand. \n 7. The game ends when one of the players loses all the lives.\n "
@@ -12,12 +12,12 @@ class Strings:
   ENTRY = "`Entry completed.\n%s: 10 Lives.`"
   GAME_STARTS = "`The game will start now.\nEach player will receive two cards.`"
   ALREADY = "`There are already two people playing.`"
-  ROUND = "`Round %i:`"
+  ROUND = "`Hand %i:`"
   CARDS = "`These are your cards.`"
   SELECT_CARD = "`Please select your card.`"
   MENU1 = "Select 1st card"
   MENU2 = "Select 2nd card"
-  CARD_SELECTED = "`{} has selected a card.`"
+  CARD_SELECTED = "`{}\'s card received.`"
   CARD_SELECTED2 = "`{} has already selected a card.`"
   SELECTION_COMPLETED = "`Card selection has been completed.\nNow proceeding to betting phase.`"
   QUESTION = "`Check or bet?`"
@@ -28,6 +28,7 @@ class Strings:
   CHECK = "`Both players check.\nNow revealing the cards.`"
   P_FOLDS = "`%s folds.`"
   P_CHECKS = "`%s checks.`"
+  P_CALLS = "`%s calls.`"
   LIVES_BET = "`%s bets %i lives.`"
   CALL_RAISE_FOLD = "`Call, raise or fold?`"
   BET_COMPL = "`Betting complete.`"
@@ -36,8 +37,8 @@ class Strings:
   P_QUITS = "`%s quits. Game ends.`"
   END = "`Game ends.`"
   R_LIFE = "`Will you bet your own life?`"
-  R_GAME = "`%s\'s own life is bet. Game continues.`"
-  SCORES = "`Round %i:\n[%s(bet: %i)-%s | %s(bet:%i)-%s]\nRESULT: %s`"
+  R_GAME = "`A red life has been detected.\n Now registering...\nGame extension confirmed! Red life accepted!\n%s\'s lives now set to one.\nNow engaging the game extension.`"
+  SCORES = "`Hand %i:\n[%s(bet: %i)-%s | %s(bet:%i)-%s]\nRESULT: %s`"
 
   #for fun
   ZAWAZAWA = "ざわ... ざわ..."
@@ -50,3 +51,4 @@ class Strings:
   CATCH = [ZAWAZAWA, NATURAL, MEAN, KUYASHII, STILL, BUT, PRESSURE, ZAWAZAWA, ZAWAZAWA]#extra zawas, just because
   ZAWA_ON = "`Zawa mode switched ON`"
   ZAWA_OFF = "`Zawa mode switched OFF`"
+  NOISE = "*kzzt*"
