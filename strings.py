43d292f7531ba502be5390fc389a00d32cf13b07
Cristina <cristina.corralm@gmail.com>
2017-12-20 06:23:40 +0100

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 class Strings:
 
-  TOKEN = "500680213:AAH6o1YqqkFHg8MyBRRKZpfQGsu_e0OZ9Jw" #write here your bot's token
+  TOKEN = "___YOURBOTSTOKEN___" #write here your bot's token
   GREETINGS = "`Welcome to One Poker. If you wish to participate, enter the command` /participate"
   GREETINGS2 = "`Type` /rules `for an explanation of the game mechanics and` /help `to know the commands of this bot. Type` /disclaimer `to know further.`"
   RULES = "One Poker is a game in which two people play following these rules:\n1. Both of you will receive 10 lives, you can bet a minimum of 1 life in each round and a maximum of the lives you have at that moment.\n2.The game uses three decks of poker without the joker card, when . \n 3. Each player will receive 2 cards and will have in his hand always 2 cards. \n 4. The value of the cards is determined by its number, being DOWN cards: 2, 3, 4, 5, 6, 7 and UP cards: 8, 9, 10, J, Q, K, A. \n 5. The winning card is always the highest with the exception of the 2 that wins the Ace. \n 6. Once players receive their cards, both of them will be informed if they have UP or DOWN cards in their hand. \n 7. The game ends when one of the players loses all the lives.\n "
