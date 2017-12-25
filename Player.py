b7a1d11218b5152dd5a734ceb5fba3e93180060b
Cristina <cristina.corralm@gmail.com>
2017-12-25 23:05:39 +0100

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,4 +1,10 @@
+# -*- encoding: utf 8 -*-
+
+
 class Player:
+    LIVES = "You: Bet🕴x%i - Lives🕴x%i   \nRival: Bet🕴x%i - Lives🕴x%i"
+    UP = "⬆️"
+    DOWN = "️⬇️"
 
     def __init__(self,
                  name,
@@ -25,19 +31,20 @@ class Player:
         self.red_lives = red_lives
         self.hand = hand
 
-    def displayStatus(self):
-        if len(self.hand) > 1:
-            return "`%s: %s | %s`" % (self.name, self.isUPorDOWN(self.hand[0]), self.isUPorDOWN(self.hand[1]))
-        else:
-            return " "
+    def displayStatus(self, rival):
+        return "You: %s %s\nRival: %s %s" % (self.isUPorDOWN(self.hand[0]), self.isUPorDOWN(self.hand[1]),
+                                            rival.isUPorDOWN(self.hand[0]), rival.isUPorDOWN(self.hand[1]))
 
     def isUPorDOWN(self, card):  # ideally, this will be in a different class named card, but... 2lazy
         if '2' in card or '3' in card or '4' in card or '5' in card or '6' in card or '7' in card:
-            return 'DOWN'
+            return Player.DOWN
         else:
-            return 'UP'
+            return Player.UP
 
     def setBet(self, quantity):
         if quantity <= self.lives:
             self.lives = self.lives - quantity
             self.bet = self.bet + quantity
+
+    def display_lives(self, rival):
+        return Player.LIVES % (self.bet, self.lives, rival.bet, rival.lives)
